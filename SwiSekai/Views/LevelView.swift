cc4ca7a080fa654552b2c5a65b717aed5767e56b
Adrian Rachman <adrian.y.rachman@gmail.com>
2025-08-13 10:13:56 +0800

    Added levels view

---
+++ SwiSekai/Views/LevelView.swift
@@ -0,0 +1,18 @@
+//
+//  LevelView.swift
+//  SwiSekai
+//
+//  Created by Adrian Yusufa Rachman on 12/08/25.
+//
+
+import SwiftUI
+
+struct LevelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LevelView()
+}
